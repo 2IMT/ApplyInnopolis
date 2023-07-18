a9c2e006809d2a602ce8803a93122d099c3a5352
2IMT <daniil_t04@mail.ru>
2023-07-18 03:38:16 +0300

    refactor: format exit.js

--- js/exit.js
+++ js/exit.js
@@ -5,12 +5,12 @@ const exitRed = document.querySelector("#button_exit_red");
 const exitGreen = document.querySelector("#button_exit_green");
 const exitIcon = document.querySelector("#exit_button");
 
-exitIcon.addEventListener('mouseover', function() {
+exitIcon.addEventListener('mouseover', function () {
     logout.style.opacity = "1";
     nameId.style.opacity = "0";
     exitRed.style.opacity = "1";
     exitGreen.style.opacity = "0";
-    if(window.innerWidth < 1024){
+    if (window.innerWidth < 1024) {
         xRay.style.right = "12px"
     } else {
         xRay.style.right = "15px"
@@ -18,12 +18,12 @@ exitIcon.addEventListener('mouseover', function() {
     xRay.style.color = "#E64622";
 });
 
-exitIcon.addEventListener('mouseout', function() {
+exitIcon.addEventListener('mouseout', function () {
     logout.style.opacity = "0";
     nameId.style.opacity = "1";
     exitRed.style.opacity = "0";
     exitGreen.style.opacity = "1";
-    if(window.innerWidth < 1024){
+    if (window.innerWidth < 1024) {
         xRay.style.right = "65px"
     } else {
         xRay.style.right = "80px"
