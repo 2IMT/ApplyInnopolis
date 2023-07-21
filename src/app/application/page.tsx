3192d41b7f952eb8c391060ea608b10cd3221eb2
2IMT <daniil_t04@mail.ru>
2023-07-21 15:01:27 +0300

    feat: use grid for some input fields

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -1,47 +1,54 @@
 import Card from '../components/card'
 import Button from '../components/button'
 import InputField from '../components/inputField'
+import Grid from '../components/grid'
 
 export default function Application() {
   return (
     <main>
       <Card heading="Personal Data" body={
         <div>
-          <InputField labelName="Educational Program" 
-                      inputName="educational-program"/>
+          <InputField labelName="Educational Program"
+            inputName="educational-program" />
 
-          <InputField labelName="First Name" 
-                      inputName="first-name"/>
+          <Grid columns={2}>
+            <InputField labelName="First Name"
+              inputName="first-name" />
 
-          <InputField labelName="Last Name" 
-                      inputName="last-name"/>
+            <InputField labelName="Last Name"
+              inputName="last-name" />
+          </Grid>
 
-          <InputField labelName="Patronymic (If any)" 
-                      inputName="patronymic"/>
+          <InputField labelName="Patronymic (If any)"
+            inputName="patronymic" />
 
-          <InputField labelName="Date Of Birth" 
-                      inputName="birthday"
-                      inputType="date"/>
+          <InputField labelName="Date Of Birth"
+            inputName="birthday"
+            inputType="date" />
 
-          <InputField labelName="Email Address" 
-                      inputName="email"
-                      inputType="email"/>
+          <Grid columns={2}>
+            <InputField labelName="Email Address"
+              inputName="email"
+              inputType="email" />
 
-          <InputField labelName="Contact Telephone Number" 
-                      inputName="number"
-                      inputType="tel"/>
+            <InputField labelName="Contact Telephone Number"
+              inputName="number"
+              inputType="tel" />
+          </Grid>
 
-          <InputField labelName="Where Did You Hear About Us?" 
-                      inputName="where-did-you-hear"/>
+          <InputField labelName="Where Did You Hear About Us?"
+            inputName="where-did-you-hear" />
 
-          <InputField labelName="Country Where You Live" 
-                      inputName="country"/>
+          <Grid columns={2}>
+            <InputField labelName="Country Where You Live"
+              inputName="country" />
 
-          <InputField labelName="City" 
-                      inputName="city"/>
+            <InputField labelName="City"
+              inputName="city" />
+          </Grid>
 
-          <InputField labelName="Citizenship" 
-                      inputName="citizenship"/>
+          <InputField labelName="Citizenship"
+            inputName="citizenship" />
 
           <br />
 
