6dc1411d57b6d17f6ae5eccc08f1aed7b1f67734
Timur Garifullin <cyber704d@gmail.com>
2023-07-21 17:13:30 +0300

    fix: rewrite exit anaimation with css to enable server-side render for component

--- src/app/components/exit.tsx
+++ src/app/components/exit.tsx
@@ -1,38 +1,6 @@
-'use client';
-
 import '../globals.css'
 import styles from '../styles/exit.module.css'
 import Image from 'next/image'
-
-function exit() {
-    const xRay = document.getElementById(styles.x_ray);
-    const logout = document.getElementById(styles.log_out);
-    const nameId = document.getElementById(styles.name_id);
-    const exitRed = document.getElementById(styles.button_exit_red);
-    const exitGreen = document.getElementById(styles.button_exit_green);
-    const exitIcon = document.getElementById(styles.exit_button);
-
-    if (xRay && logout && nameId && exitRed && exitGreen && exitIcon)
-    {
-      exitIcon.addEventListener('mouseover', function() {
-        logout.style.opacity = "1";
-        nameId.style.opacity = "0";
-        exitRed.style.opacity = "1";
-        exitGreen.style.opacity = "0";
-        xRay.style.right = "20px"
-        xRay.style.color = "#E64622";
-      });
-    
-      exitIcon.addEventListener('mouseout', function() {
-          logout.style.opacity = "0";
-          nameId.style.opacity = "1";
-          exitRed.style.opacity = "0";
-          exitGreen.style.opacity = "1";
-          xRay.style.right = "70px"
-          xRay.style.color = "#40BA21";
-      });
-    }
-}
 
 export default function Exit() {
   return (
@@ -45,8 +13,7 @@ export default function Exit() {
             </div>
             <div className={styles.log_out} id={styles.log_out}>Log out</div>
         </div>
-        <div className={styles.exit_button} id={styles.exit_button} 
-        onMouseOver={exit}>
+        <div className={styles.exit_button} id={styles.exit_button}>
             <div id={styles.x_ray}></div>
             <Image alt="exit" width={30} height={30} id={styles.button_exit_green} src="/images/exit_button.svg" />
             <Image alt="exit" width={30} height={30} id={styles.button_exit_red} src="/images/exit_button_red.svg" />
