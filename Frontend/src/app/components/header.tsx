d137b9ab504fb7e3554487221f2688c1a5e0e8bf
2IMT <daniil_t04@mail.ru>
2023-07-22 23:45:18 +0300

    fix: use React Image instead of img in header.tsx

--- Frontend/src/app/components/header.tsx
+++ Frontend/src/app/components/header.tsx
@@ -2,13 +2,14 @@ import '../globals.css'
 import styles from '../styles/header.module.css'
 import Exit from './exit'
 import MenuTrigger from './menuTrigger'
+import Image from 'next/image'
 
-export default function () {
+export default function Header() {
     return (
         <header>
             <div id={styles.header_desktop}>
                 <div>
-                    <img className={styles.img_innopolis} src="/images/innopolis-university-logo-full-en_green.png" alt="" />
+                    <Image className={styles.img_innopolis} src="/images/innopolis-university-logo-full-en_green.png" alt=""/>
                 </div>
                 <div id={styles.enrollment_status}>
                     Application status: <div>Enrolled on partial scholarship</div>
@@ -18,7 +19,7 @@ export default function () {
             <div id={styles.header_mobile}>
                 <MenuTrigger />
                 <div>
-                    <img style={{margin: "0 auto"}} className={styles.img_innopolis} src="/images/innopolis-university-logo-full-en_green.png" alt="" />
+                    <Image style={{margin: "0 auto"}} className={styles.img_innopolis} src="/images/innopolis-university-logo-full-en_green.png" alt="" />
                 </div>
                 <Exit />
             </div>
