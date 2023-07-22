13dfe1b6c9d4774cc6c0b7a388db03a6fe86009f
2IMT <daniil_t04@mail.ru>
2023-07-22 23:41:57 +0300

    fix: use interface for props in InputField class

--- Frontend/src/app/components/inputField.tsx
+++ Frontend/src/app/components/inputField.tsx
@@ -4,25 +4,27 @@ import '../globals.css'
 import styles from '../styles/inputField.module.css'
 import React from 'react'
 
-export default class InputField extends React.Component {
+export interface InputFieldProps {
+    labelName: string;
+    inputName: string;
+    inputPlaceholder?: string;
+    inputType?: string;
+}
+
+export default class InputField extends React.Component<InputFieldProps, {}> {
     labelName: string;
     inputName: string;
     inputPlaceholder: string;
     inputType: string;
 
-    constructor(props: Readonly<{
-        labelName: string,
-        inputName: string,
-        inputPlaceholder: string,
-        inputType: string
-    }>) {
+    constructor(props: InputFieldProps) {
         super(props);
         this.setState({ value: "" });
 
         this.labelName = props.labelName;
         this.inputName = props.inputName;
-        this.inputPlaceholder = props.inputPlaceholder;
-        this.inputType = props.inputType;
+        this.inputPlaceholder = props.inputPlaceholder == null ? "" : props.inputPlaceholder;
+        this.inputType = props.inputType == null ? "text" : props.inputType;
     }
 
     render() {
