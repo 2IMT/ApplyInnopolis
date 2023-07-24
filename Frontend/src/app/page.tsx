389ee6288274f4f211956dabb2222f3be9ecb334
2IMT <daniil_t04@mail.ru>
2023-07-24 21:45:44 +0300

    fix: catch fetching errors

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -15,43 +15,52 @@ interface FetchedTests {
 function Test(test: FetchedTest) {
   return (
     <Card heading={test.Name} body={<span>
-      Time: {test.Time} min<br/>
-      Attempts: {test.Attempts}<br/>
+      Time: {test.Time} min<br />
+      Attempts: {test.Attempts}<br />
       <Button text="START" inputType="button" />
     </span>} />
   )
 }
 
 function fetchTests() {
-  
-  return fetch('https://cybertoad.ru/api/tests/getTests', {cache: "no-store"})
+
+  return fetch('https://cybertoad.ru/api/tests/getTests', { cache: "no-store" })
     .then(response => response.json())
     .then(response => {
       return response as FetchedTests;
     })
 }
 
-async function Tests()
-{
-  const tests = await fetchTests();
+async function Tests() {
+  try {
+    const tests = await fetchTests();
 
-  tests.Tests.sort((a, b) => (a.Time - b.Time));
+    tests.Tests.sort((a, b) => (a.Time - b.Time));
 
-  let testsList: JSX.Element[] = [];
+    let testsList: JSX.Element[] = [];
 
-  for (let i = 0; i < tests.Tests.length; i++) {
-    const element = Test(tests.Tests[i]);
+    for (let i = 0; i < tests.Tests.length; i++) {
+      const element = Test(tests.Tests[i]);
 
-    testsList.push(element);
+      testsList.push(element);
+    }
+
+    return testsList;
   }
-
-  return testsList;
+  catch (e)
+  {
+    return (
+      <main>
+        Could not fetch tests.
+      </main>
+    )
+  }
 }
 
 export default function Home() {
   return (
     <main>
-      <Tests/>
+      <Tests />
     </main>
   )
 }
