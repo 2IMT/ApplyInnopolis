742466fac95e2bd2a3674015af09236d0fdaa360
2IMT <daniil_t04@mail.ru>
2023-07-25 02:20:45 +0300

    feat: button click callback

--- Frontend/src/app/components/button.tsx
+++ Frontend/src/app/components/button.tsx
@@ -1,14 +1,15 @@
 import '../globals.css'
 import styles from '../styles/button.module.css'
 
-export default function Button({ text, inputType, link }: {
+export default function Button({ text, inputType, link, clickCallback}: {
     text: string,
     inputType: string
     link?: string
+    clickCallback?: () => void
 }) {
     return (
         <a href={link == null ? "#" : link}>
-            <input type={inputType} value={text} className={styles.button} style={{ width: "fit-content" }} />
+            <input type={inputType} value={text} className={styles.button} style={{ width: "fit-content" }} onClick={clickCallback} />
         </a>
 
     )
