f81d74cc89e77a39f1bf7edf0612fba1f17573a2
2IMT <daniil_t04@mail.ru>
2023-07-25 08:30:03 +0300

    feat: add settings page

---
+++ Frontend/src/app/settings/page.tsx
@@ -0,0 +1,15 @@
+import Card from "../components/card"
+import Button from "../components/button"
+import InputField from "../components/inputField"
+
+export default function Settings() {
+    return (
+        <main>
+            <form>
+                <InputField labelName="Password:" inputName="password" />
+                <InputField labelName="Confirm New Password:" inputName="confirm_new_password" />
+                <Button text="SAVE" inputType="submit" />
+            </form>
+        </main>
+    )
+}
